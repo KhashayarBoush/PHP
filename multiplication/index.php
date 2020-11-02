8410b3a06089fcb9a2e4855a6c9b39a9c2a0ad11
khashayarboush <khashayar.boush@icloud.com>
2020-11-02 16:35:32 +0330

    Add Comments To File

--- multiplication/index.php
+++ multiplication/index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+############ Start Create Session ###############
   session_start();
   setcookie(session_name(),session_id(),time() + 60);
 
@@ -11,7 +12,9 @@
 
   }
   */
+  ############ END Create Session ###############
 
+ ############ Start AUTH Statmnet ###############
   if(isset($_POST['username']) && !empty($_POST['username'])){
     $username = $_POST['username'];
     //$email = $_POST['email'];
@@ -27,7 +30,9 @@
             </div>
     </div>";
   }
+ ############ END AUTH Statmnet ###############
 
+ ############ Start Session Statmnet For AUTH ###############
 
   if(isset($_SESSION['username'])){
     $username = $_SESSION['username'];
@@ -40,6 +45,9 @@
     $back = false;
     $title = "Wellcome Guest";
   }
+   ############ END Session Statmnet For AUTH ###############
+
+ ############ Start LOGOUT AUTH With END OF SESSION ###############
 
   if (isset($_POST['Logout'])) {
     session_unset();
@@ -51,32 +59,48 @@
             </div>
     </div>";
   }
+   ############ END LOGOUT AUTH With END OF SESSION ###############
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Start META TAGS -->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo "$title" ?></title>
+  <!-- END META TAGS -->
+
+  <!-- Start Title TAGS -->
+	<title>
+    <?php echo "$title" ?> 
+  </title>
+  <!-- END Title TAGS -->
+
+  <!-- Start Bootstarp TAGS -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
+  <!-- END Bootstarp TAGS -->
 </head>
-<body>
 
+<body>
+<!-- START Crate Form if User login -->
   <?php if($back): ?>
   <nav class="navbar sticky-top navbar-dark bg-warning">
   <a class="navbar-brand" href="index.php"><h6 class="text-capitalize text-dark">Wellcome <?php echo "<h6 class='text-dark'><strong>Hola Hola { $username } !! </strong></h6>"; ?> </h6></a>
   <form action="index.php" method="post">
     <input type="submit" name="Logout" value="Logout">
   </form>
-   
 </nav>
 <div class="alert alert-success" role="alert">
     <strong>Well done!</strong> You Are Logedin .
 </div>
+<!-- END Crate Form if User login -->
+
+<!-- START Crate Form if User NOT LOGIN -->
 <?php else: ?>
 <nav class="navbar sticky-top navbar-dark bg-warning">
   <a class="navbar-brand" href="index.php"><h6 class="text-capitalize text-dark">Wellcome</h6></a>
@@ -100,13 +124,15 @@
   </div>
   <div class="form-group row">
     <div class="col-sm-10">
-      <button type="submit" class="btn btn-warning">Sign in</button>
+      <button type="submit" class="btn btn-warning">LogIn</button>
     </div>
   </div>
 </form>
   </div>
 <?php endif; ?>
+<!-- END Crate Form if User NOT LOGIN -->
 
+<!-- START Crate Form For Multiplation * -->
 <div class="container">
     <div class="row">
       <div class=" border-left border-right border-dark  py-2 col-6">
@@ -131,6 +157,9 @@
     </div>
   </div>
 </form>
+<!-- END Crate Form For Multiplation * -->
+
+<!-- START Crate Form For Sum Numbers + -->
 	</div>
   <div class="col-6 border-right border-dark  py-2 ">
     <form action="result-sum.php" method="post">
@@ -156,7 +185,9 @@
   </div>
 </div>
 </div>
+<!-- END Crate Form For Sum Numbers + -->
 
+<!-- START Crate Form For Incremnt Numbers - -->
 <div class="container mt-5">
     <div class="row">
  <div class="col-6 border-left border-right border-dark  py-2 ">
@@ -181,6 +212,9 @@
   </div>
 </form>
   </div>
+  <!-- END Crate Form For Incremnt Numbers - -->
+
+  <!-- START Crate Form For Module Numbers % -->
   <div class="col-6 border-right border-dark  py-2 ">
     <form action="result-div.php" method="post">
   <div class="form-row align-items-center">
@@ -203,8 +237,9 @@
   </div>
 </form>
   </div>
-</div>
-</div>
+    <!-- END Crate Form For Module Numbers % -->
+</div> <!-- END OF ROW Div-->
+</div> <!-- END OF Container Div-->
 </body>
 </html>
 
