6103c1cec3d798b83826432ce75c9ada8d0a2bb9
khashayarboush <khashayar.boush@icloud.com>
2020-11-02 16:17:51 +0330

    Add Logout Button - Add Title Box - Add Spiner ContDownTimer To UI

--- multiplication/index.php
+++ multiplication/index.php
@@ -1,30 +1,56 @@
 <?php
 
+
+  session_start();
+  setcookie(session_name(),session_id(),time() + 60);
+
+  /*
   if (!isset($_GET['checkcookie'])){
 
     header('Location: index.php?checkcookie');
 
   }
+  */
 
   if(isset($_POST['username']) && !empty($_POST['username'])){
     $username = $_POST['username'];
     //$email = $_POST['email'];
-    setcookie('username',$username,time() + 20);
+    //('username',$username,time() + 20);
     //setcookie('email',$email,time() + 60);
-    $_COOKIE['username'] = $username;
+    $_SESSION['username'] = $username;
     //$_COOKIE['email'] = $email;
+    header('Refresh: 1; url=index.php');
+    echo "<div class='alert alert-success' role='alert'>
+    <strong>Well done!</strong> You Are Login NOW .
+    <div class='spinner-border' role='status'>
+              <span class='sr-only'>Loading...</span>
+            </div>
+    </div>";
   }
 
-  if(isset($_COOKIE['username'])){
-    $username = $_COOKIE['username'];
+
+  if(isset($_SESSION['username'])){
+    $username = $_SESSION['username'];
     //$email = $_COOKIE['email'];
     $back = true;
+    $title="Wellcome $username";
   } else{
     $username = '';
     //$email = '';
     $back = false;
+    $title = "Wellcome Guest";
   }
 
+  if (isset($_POST['Logout'])) {
+    session_unset();
+    header('Refresh: 1; url=index.php');
+    echo "<div class='alert alert-danger' role='alert'>
+    <strong>Well done!</strong> You Are LogOUT NOW .
+    <div class='spinner-border' role='status'>
+              <span class='sr-only'>Loading...</span>
+            </div>
+    </div>";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -32,17 +58,21 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Dvider</title>
+	<title><?php echo "$title" ?></title>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
 </head>
 <body>
 
   <?php if($back): ?>
   <nav class="navbar sticky-top navbar-dark bg-warning">
   <a class="navbar-brand" href="index.php"><h6 class="text-capitalize text-dark">Wellcome <?php echo "<h6 class='text-dark'><strong>Hola Hola { $username } !! </strong></h6>"; ?> </h6></a>
+  <form action="index.php" method="post">
+    <input type="submit" name="Logout" value="Logout">
+  </form>
+   
 </nav>
 <div class="alert alert-success" role="alert">
     <strong>Well done!</strong> You Are Logedin .
