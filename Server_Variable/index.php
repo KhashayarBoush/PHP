6337f691bb6308341eef398d8302d1d4a1844f91
khashayarboush <khashayar.boush@icloud.com>
2020-12-18 14:47:08 +0330

    Add new project

---
+++ Server_Variable/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+	echo "<pre>";	
+	print_r ($_SERVER);
+	echo "<pre>";
+
+	// find ip whith Server Reamot Address For use Ip finder 
+	// use for another scripts	
+	
+	echo "<p>";
+	echo 'server ip ==> ' . $_SERVER['REMOTE_ADDR'];
+	echo "</p>";
+?>
